472e770160f2e5dd539fff7e96896e13b23f7073
pinkanpriharini <126873421+pinkanpriharini@users.noreply.github.com>
2024-10-25 18:46:40 +0700

    ttt

--- controllers/ControllersProduk.js
+++ controllers/ControllersProduk.js
@@ -1,1 +1,32 @@
-const produk = require("..")
+const produk = require("./models/produk");
+
+const produkIndex = async (req, res)=> {
+    try{
+        const produk = await produk.find({});
+        res.status(200).json({message: 'success'}).json(produk);
+
+    } catch (error) {
+        res.status(400).json({message: 'failed'});
+    }
+}
+
+//fungsi untukmemasukan data produk
+const produkInsert = async (req,res) => {
+    try{
+        const {nama,price,category} = req.body;
+
+        //buat instance produk baru
+        const produk = new produk({
+            nama,
+            deskripsi,
+            harga,
+            stok,
+            kategori_id
+        });
+        await produk.save();
+        res.status(200).json({message: 'success',data: produk});
+    } catch( error){
+        res.status(400).json({mssage: "failed"});
+    }
+};
+module.exports = { produkIndex, produkInsert}
